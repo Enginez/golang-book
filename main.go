95d33871f7f3c891cd728d8652fb314ddc9f17e6
cernov.e83@gmail.com <you@example.com>
2020-02-22 23:53:05 +0200

    multi gorouting

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-func f(n int) {
-    for i := 0; i < 10; i++ {
+func a(n int) {
+    go b(n-1)
+    for i := 0; i < n; i++ {
         fmt.Println(n, ":", i)
+        
+        
+    }
+}
+
+func b(n int) {
+    go a(n-1)
+    for i := 0; i < n; i++ {
+        fmt.Println(n, ":", i)
+  
     }
 }
 
 
 func main() {
-    go f(0)
+    go a(5)
     var input string
     fmt.Scanln(&input)
 }
